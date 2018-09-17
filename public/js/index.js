619d0249d71e750bc5bf1ff10300cbd742531863
Vinz Apside <vinzapside@gmail.com>
2018-09-17 11:33:59 +0200

    mise en place de socket.io

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+let socket = io() 
+//  permet de faire connexion entre front et back
+socket.on('connect', () => {
+  console.log(' the user is connected')
+
+  socket.emit('createEmail', {
+    to: 'jen@example.com',
+    text: "hey, it's me !"
+  })
+})
+socket.on('disconnect', () => {
+  // bug possible en mobile si on utilise fonction flechee plutot que function
+  console.log('disconnected from server')
+})
+
+socket.on('newEmail', email => {
+  // on cree un evenement newEmail
+  console.log('new email', email)
+
+  socket.emit('createMessage', {
+    from: 'the user',
+    message: 'hello, is it me you looking for ?'
+  })
+})
+
+socket.on('newMessage', message => {
+  console.log('newMessage', message)
+})
--- server/server.js
+++ server/server.js
@@ -1,16 +1,47 @@
-const path = require("path");
-const express = require('express');
+const path = require('path')
+const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
-const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
-const app = express();
-
-// app.get("/", (req, rep) => {
-//      rep.send("hello")
-//    });
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+io.on('connection', socket => {
+  console.log('new user connected')
+
+  socket.on('disconnect', () => {
+    console.log('the user is disconnected')
+  })
+
+  socket.emit('newEmail', {
+    // on recupere l'evenement créer dans le js
+    from: 'mike@example.com',
+    text: "hey, what's going on",
+    createAt: 123
+  })
+
+
+  socket.on('createEmail', newEmail => {
+    console.log('createEmail', newEmail)
+  })
+
+  socket.on("createMessage", (message)=>{
+    console.log('create message :', message)
+  })
+
+  socket.emit('newMessage',
+  {
+    from: 'john',
+    text:"see you there",
+    createdAt : 123
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`)
 })
