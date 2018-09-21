b8e704664cf3670add1a5a59590b54c2d199b24e
Vinz Apside <vinzapside@gmail.com>
2018-09-21 11:14:10 +0200

    learn about timestamp

---
+++ playground/time.js
@@ -0,0 +1,23 @@
+// let date = new Date();
+
+// console.log(date.getMonth());
+
+const moment = require('moment');
+
+let date = moment();
+
+console.log(date.format('mm MMM YY')); //permet de voir le mois  sous forme text abrégé (3m=3lettres) et année actuel
+
+console.log(date.format('Do MMM, YYYY'));
+
+let dateModif = moment();
+dateModif.add(1, 'year').subtract(9, 'month');
+console.log(dateModif.format('Do MMM YYYY'));
+
+
+//10:35 am format en 6:01 am
+
+let dateTest = moment();
+
+dateTest.subtract(4, 'hours').subtract(34, 'minutes')
+console.log(dateTest.format('h:mm a'));
