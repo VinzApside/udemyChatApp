63fd89d5314c5b089a526d18dca48f8adaca4f2c
Vinz Apside <vinzapside@gmail.com>
2018-09-24 11:54:08 +0200

    remove & get

--- server/utils/users.js
+++ server/utils/users.js
@@ -7,6 +7,23 @@ class Users {
     this.users.push(user);
     return user;
   }
+
+  removeUser(id) {
+    const users = this.users.filter((user) => user.id !== id);
+    const namesArray = users.map((user)=> user.name);
+    return namesArray; 
+  }
+
+  getUser(id) {
+    const findUser = this.users.filter((user) => user.id === id);
+    return findUser[0];
+  }
+
+  getUsersList(room) {
+    const users = this.users.filter((user) => user.room === room);
+    const namesArray = users.map((user) => user.name);
+    return namesArray;
+  }
 }
 
 module.exports = { Users };
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,14 +2,61 @@ const expect = require("expect");
 const { Users } = require("./users");
 
 describe("Users", () => {
+
+  let users;
+  users = new Users();
+
+  users.users = [
+    {
+      id: "1",
+      name: "a",
+      room: 'node course'
+    },
+    {
+      id: "2",
+      name: "b",
+      room: 'react course'
+    },
+    {
+      id: "3",
+      name: "c",
+      room: 'node course'
+    }
+  ]
+
   it("Should add new user", () => {
-    let newUsers = new Users();
+    let users = new Users();
     let user = {
       id: 123,
       name: "Vinz",
       room: "The_office"
     };
-    const result = newUsers.addUser(user.id, user.name, user.room);
-    expect(newUsers.users).toEqual([user]);
+    const result = users.addUser(user.id, user.name, user.room);
+    expect(users.users).toEqual([user]);
   });
+
+  it("should remove a user", () => {
+    const usersWithout = users.removeUser('1');
+    expect(usersWithout).toEqual(["b", "c"])
+  })
+
+  it("should not remove user", () => {
+    const usersWithout = users.removeUser('0');
+    expect(usersWithout).toEqual(["a", "b", "c"])
+  })
+
+  it("should find a user", () => {
+    const userFinded = users.getUser("1");
+    expect(userFinded.name).toEqual("a")
+  })
+
+  it("should not find a user", () => {
+    const userFinded = users.getUser("0");
+    expect(userFinded).toEqual(undefined)
+  })
+
+  it("should return names for node course", () => {
+    const userList = users.getUsersList('node course');
+    expect(userList).toEqual(['a', 'c']);
+  })
 });
